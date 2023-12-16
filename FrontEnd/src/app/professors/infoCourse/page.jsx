01d83ad0214e33e1710953216801653c70908a06
Alexandru Stelea <alexandrustelea@yahoo.com>
2023-12-16 02:05:53 +0200

    Small fix to infoCourse page regarding the rendering of components.

--- FrontEnd/src/app/professors/infoCourse/page.jsx
+++ FrontEnd/src/app/professors/infoCourse/page.jsx
@@ -1,15 +1,15 @@
-'use client'
+"use client";
 import Chart from "@/components/infoCourse/Chart";
 import RatingOverview from "@/components/infoCourse/RatingOverview";
 import LikeDislike from "@/components/infoCourse/LikeDislike";
-import { useSearchParams } from 'next/navigation'
-import styles from './page.module.css'
-import React, { useState } from 'react';
+import { useSearchParams } from "next/navigation";
+import styles from "./page.module.css";
+import React, { useState } from "react";
 
 export default function InfoCourse() {
-  const [isError404, setIsError404] = useState(false);
-  const searchParams = useSearchParams()
-  const subjectId = searchParams.get('subjectId')
+  const [isError404, setIsError404] = useState(true);
+  const searchParams = useSearchParams();
+  const subjectId = searchParams.get("subjectId");
 
   const handleError = (error) => {
     if (error) {
@@ -17,20 +17,19 @@ export default function InfoCourse() {
     }
   };
 
-
   return (
     <>
-      {isError404 ? <div className={styles.noFoundContainer}>NU EXISTĂ DATE!</div> : (
+      {isError404 ? (
+        <div className={styles.noFoundContainer}>NU EXISTĂ DATE!</div>
+      ) : (
         <div className={styles.infoCourseContainer}>
-
           <Chart onError={handleError} subjectId={subjectId} />
           <div className={styles.container}>
             <RatingOverview subjectId={subjectId} onError={handleError} />
             <LikeDislike subjectId={subjectId} />
           </div>
         </div>
-      )
-      }
+      )}
     </>
-  )
+  );
 }
