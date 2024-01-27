2320409bacd7e59ca7322835ce924db6182779dc
savaandrei123 <savacristianandrei@yahoo.com>
2024-01-27 14:55:29 +0200

    Changed code for google analytics

--- FrontEnd/next.config.js
+++ FrontEnd/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     REACT_APP_API_URL: '/api',    // REACT_APP_API_URL: 'http://127.0.0.1:5000',
-    GOOGLE_ANALYTICS: 'G-S02J90JMSB',
+    GOOGLE_ANALYTICS: 'G-VPQDW8R82P',
   },
 };
 
