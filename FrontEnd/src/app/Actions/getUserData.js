2785e35e291bda35812b498bf36ee19945d1364b
savaandrei123 <savacristianandrei@yahoo.com>
2024-04-10 17:42:29 +0300

    Completed logic for login and voting page.

--- FrontEnd/src/app/Actions/getUserData.js
+++ FrontEnd/src/app/Actions/getUserData.js
@@ -11,3 +11,22 @@ export const fetchCheckLogin = async () => {
 
   return loggedIn;
 };
+
+export const fetchLogoutUser = async () => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/logout`, {
+      method: "GET",
+      credentials: "include",
+    });
+    if (response.ok) {
+      console.log("Logout successful");
+      return true;
+    } else {
+      console.error("Logout failed:", response.statusText);
+      return false;
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
