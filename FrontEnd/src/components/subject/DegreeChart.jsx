a064cafd9cae3647f784e6da3a6b21159985da28
savaandrei123 <savacristianandrei@yahoo.com>
2024-04-21 01:50:52 +0300

    Styled the degree chart when there are no grades

--- FrontEnd/src/components/subject/DegreeChart.jsx
+++ FrontEnd/src/components/subject/DegreeChart.jsx
@@ -5,30 +5,38 @@ import { Pie } from "react-chartjs-2";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function DegreeChart() {
+  const dataAvailable = false;
   const data = {
-    labels: ["1-4", "5-6", "7-8", "9-10"],
+    labels: dataAvailable ? ["1-4", "5-6", "7-8", "9-10"] : ["Nu există date"],
     datasets: [
       {
         label: "note",
-        data: [12, 19, 3, 5],
-        backgroundColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(54, 162, 235, 1)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(54, 162, 235, 1)",
-        ],
+        data: dataAvailable ? [12, 19, 3, 5] : [1],
+        backgroundColor: dataAvailable
+          ? [
+              "rgba(255, 99, 132, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(54, 162, 235, 1)",
+            ]
+          : ["rgba(192, 192, 192, 1)"],
+        borderColor: dataAvailable
+          ? [
+              "rgba(255, 99, 132, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(54, 162, 235, 1)",
+            ]
+          : ["rgba(192, 192, 192, 1)"],
         borderWidth: 0.5,
       },
     ],
   };
   const options = {
     plugins: {
+      tooltip: {
+        enabled: dataAvailable ? true : false,
+      },
       legend: {
         position: "bottom",
         labels: {
