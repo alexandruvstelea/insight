23785d6e5260907c6a782a272fd2dbe4e4aa2d8a
alexandruvstelea <alexandrustelea@yahoo.com>
2024-04-21 19:00:14 +0300

    Modified comments model structure.

--- Backend/models/comments.py
+++ Backend/models/comments.py
@@ -5,18 +5,15 @@ class Comment(db.Model):
     __tablename__ = "Comments"
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String)
-    is_anonymous = db.Column(db.Boolean)
     subject_id = db.Column(db.Integer, db.ForeignKey("Subjects.id"))
     datetime = db.Column(db.DateTime(timezone=False))
 
     def __init__(
         self,
         comment: str,
-        is_anonymous: bool,
         subject_id: int,
         datetime,
     ):
         self.comment = comment
-        self.is_anonymous = is_anonymous
         self.subject_id = subject_id
         self.datetime = datetime
