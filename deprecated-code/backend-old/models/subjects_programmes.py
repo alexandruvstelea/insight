a31ad310eb9d5a2b1beb02b04f9a2194cc626332
alexandruvstelea <alexandrustelea@yahoo.com>
2024-07-27 23:31:28 +0300

    Deleted old/unused code.

--- deprecated-code/backend-old/models/subjects_programmes.py
+++
@@ -1,9 +0,0 @@
-from __init__ import db
-
-subjects_programmes = db.Table(
-    "Subjects_Programmes",
-    db.Column("subject_id", db.Integer, db.ForeignKey("Subjects.id"), primary_key=True),
-    db.Column(
-        "programme_id", db.Integer, db.ForeignKey("Programmes.id"), primary_key=True
-    ),
-)
