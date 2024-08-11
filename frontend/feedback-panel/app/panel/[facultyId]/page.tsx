4bc894e9a2ec5a59b5c849444f686f5ccd7f473c
alexandruvstelea <alexandrustelea@yahoo.com>
2024-08-11 19:51:23 +0300

    Worked on landing page.

---
+++ frontend/feedback-panel/app/panel/[facultyId]/page.tsx
@@ -0,0 +1,18 @@
+import { NavigationBar } from "@/components/navigationBar/page";
+
+interface SearchParams {
+  facultyName?: string;
+}
+
+interface PanelProps {
+  searchParams: SearchParams;
+}
+
+export default function Panel({ searchParams }: PanelProps) {
+  const facultyName = searchParams.facultyName || "";
+  return (
+    <>
+      <NavigationBar facultyName={facultyName} />
+    </>
+  );
+}
---
+++ frontend/feedback-panel/components/navigationBar/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+
+export function NavigationBar() {
+  return (
+    <div className={styles.navBar}>
+      <h1>inSight</h1>
+    </div>
+  );
+}
