9b099e711062f7c21997399649ce5a197a763f5c
alexandruvstelea <alexandrustelea@yahoo.com>
2024-10-30 09:08:51 +0200

    Added HTML attribute.

--- frontend/feedback-panel/app/layout.tsx
+++ frontend/feedback-panel/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <html lang="en">
+      <html lang="en" interactive-widget="resizes-content">
         <body className={`${montserrat.className} layoutBody`}>
           <div className="layoutContainer">
             {children}
