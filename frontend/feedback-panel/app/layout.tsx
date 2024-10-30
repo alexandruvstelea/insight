be4f834ab0fbc5595c0043903f03696858434bef
alexandruvstelea <alexandrustelea@yahoo.com>
2024-10-30 09:30:24 +0200

    Added HTML attributes.

--- frontend/feedback-panel/app/layout.tsx
+++ frontend/feedback-panel/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <head>
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
           />
         </head>
         <body className={`${montserrat.className} layoutBody`}>
