aedf2444fcdc308b3c71c8394bea6d394d20c5a5
alexandruvstelea <alexandrustelea@yahoo.com>
2024-10-30 20:17:48 +0200

    Trying to forward IP from server side.

--- frontend/feedback-panel/utils/fetchers/faculties.ts
+++ frontend/feedback-panel/utils/fetchers/faculties.ts
@@ -1,19 +1,42 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
 const API_URL = process.env.API_URL;
 
-export const fetchFaculties = async () => {
-  const response = await fetch(`${API_URL}/faculties`, {
+export const fetchFaculties = async (req: NextApiRequest) => {
+  let clientIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+  if (Array.isArray(clientIp)) {
+    clientIp = clientIp.join(", ");
+  } else if (clientIp === undefined) {
+    clientIp = "";
+  }
+
+  const response = await fetch(`${process.env.API_URL}/faculties`, {
     method: "GET",
     cache: "no-store",
     headers: {
       "Content-Type": "application/json",
+      "X-Forwarded-For": clientIp,
     },
   });
+
   if (!response.ok) return false;
+
   const faculties = await response.json();
   return faculties;
 };
 
-export const fetchFaculty = async (facultyAbbreviation: string) => {
+export const fetchFaculty = async (
+  req: NextApiRequest,
+  facultyAbbreviation: string
+) => {
+  let clientIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+  if (Array.isArray(clientIp)) {
+    clientIp = clientIp.join(", ");
+  } else if (clientIp === undefined) {
+    clientIp = "";
+  }
   const response = await fetch(
     `${API_URL}/faculties/abbreviation/${facultyAbbreviation}`,
     {
@@ -21,6 +44,7 @@ export const fetchFaculty = async (facultyAbbreviation: string) => {
       cache: "no-store",
       headers: {
         "Content-Type": "application/json",
+        "X-Forwarded-For": clientIp,
       },
     }
   );
